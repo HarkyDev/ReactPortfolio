77ee1c92359779d5408147d1f898d5575127b4f1
Eoin Harkin <eharky@gmail.com>
2022-04-10 23:29:34 +0100

    updated project json

--- src/src/myProjects.js
+++ src/src/myProjects.js
@@ -1,40 +1,47 @@
 const projects = [
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
+    name: "ReactPortolio",
+    about: "The idea for this project was to put together a working, professional and practical portfolio whilst also demonstrating my ability to generate the front end using react and hosting it on a remote server. This project also shows my ability to integrate other front end technologies along side react.",
+    git: "https://github.com/HarkyDev/ReactPortfolio",
+    site:""
   },
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
+    name: "TextEditPWA",
+    about: "This is a PWA project and more of debugging practice of fixing base code that was faulty and not up to the standards of PWA to get this project functioning it required me to follow the PWA standards and systematically debug the codebase",
+    git: "https://github.com/HarkyDev/TextEditPWA",
+    site:"https://pwa-text-application.herokuapp.com/"
   },
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
+    name: "MongoCRUD",
+    about: "The idea of this project is to show I could set up the CRUD system like I have in the past with MySQL but in this instance how to update a MongoDb database with url based Crud requests.",
+    git: "https://github.com/HarkyDev/MongoCrud",
+    site:""
   },
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
+    name: "Legogram (instaClone)",
+    about: "This Is a clone of instagram based around the idea of a niche centred theme, in this case we chose lego. The project was designed to show the use of MySQL and how we handle backend data requests through custom built api paths and also server up the front end with Handlebars following the MVC model",
+    git: "https://github.com/HarkyDev/Leg-it",
+    site:""
   },
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
+    name: "E-Commerce back end",
+    about: "This project was built to demonstrate how I could repair already given code into a working database hosted on a server(express) and have it be manageable using url requests.  ",
+    git: "https://github.com/HarkyDev/E-Commerce-Client",
+    site:""
   },
   {
-    name: "MongoDB Crud",
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    git: "https://www.github.com",
-    site:"https://www.google.co.uk/"
-  }
+    name: "SQL team builder",
+    about: " This project is designed to create database that allows a user to add and or view mulitple fields, entries and users to a table using node. The User can go through a menu that will allow them to select said options and then user and run multiple commands to execute these options",
+    git: "https://github.com/HarkyDev/SQL-TeamBuilder",
+    site:""
+  },
+  {
+    name: "FindMyFootball",
+    about: "FindMyFootball is an api based project that requires football information from teams to the front end and serve up to the user, from multiple leagues at the highest professional level of the sport.",
+    git: "https://github.com/HarkyDev/FindMyFootballApp",
+    site:"https://harkydev.github.io/FindMyFootballApp/"
+  },
+
 
 ];
 
